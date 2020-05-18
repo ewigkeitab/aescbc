d814bc532a4eaf0970ca171911c22e82495db21e
fallinginmyhand@gmail.com <fallinginmyhand@gmail.com>
2020-05-18 16:45:05 +0800

    first commit

---
+++ aescbc.go
@@ -0,0 +1,29 @@
+package aescbc
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fallinglib/paddingworker"
+)
+
+// AESCBCenc AES CBC enc
+func AESCBCenc(src []byte, key []byte, iv []byte, blkSize int) []byte {
+
+	padsrc, newlen := paddingworker.PCKS(src, blkSize)
+	blk, _ := aes.NewCipher(key)
+	ensrc := make([]byte, newlen)
+	mode := cipher.NewCBCEncrypter(blk, iv)
+	mode.CryptBlocks(ensrc, padsrc)
+	return ensrc
+}
+
+// AESCBCdec AES CBC dec
+func AESCBCdec(src []byte, key []byte, iv []byte, blkSize int) []byte {
+
+	blk, _ := aes.NewCipher(key)
+	ensrc := make([]byte, len(src))
+	mode := cipher.NewCBCDecrypter(blk, iv)
+	mode.CryptBlocks(ensrc, src)
+	rt := paddingworker.RemovePadding(ensrc)
+	return rt
+}
